a070a70ea63b7a410477c65cf89492c7309d7fe8
David Alimi <davealimi@gmail.com>
2024-04-02 08:20:12 +0300

    Adding SSL

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -2,3 +2,22 @@ echo 'vm.max_map_count=512000' >> /etc/sysctl.conf && sysctl -p
 
 mkdir -p ./data;
 chown -R 1000:1000 ./data;
+
+
+openssl genrsa -out ./data/root-ca-key.pem 2048
+openssl req -new -x509 -sha256 -key ./data/root-ca-key.pem -out ./data/root-ca.pem -days 3650 -subj "/CN=${DOMAIN}"
+
+# Add the required configuration to opensearch.yml
+cat <<EOF >> ./opensearch.yml
+plugins:
+  security:
+    ssl:
+      transport.enforce_hostname_verification: false
+      http.enabled: true
+      http.pemtrustedcas_filepath: root-ca.pem
+    authcz:
+      admin_dn:
+        - 'CN=${DOMAIN}'
+    nodes_dn:
+      - 'CN=${DOMAIN}'
+EOF
